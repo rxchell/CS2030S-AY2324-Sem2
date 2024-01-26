710648a7a61edb2a10f4a1b2fb53e3f4ae677631
Rachel <133939424+rxchell@users.noreply.github.com>
2024-01-26 17:37:32 +0800

    Create Point.java

---
+++ lab/lab01/Point.java
@@ -0,0 +1,21 @@
+class Point {
+  private double x;
+  private double y;
+
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+  public double getX() {
+    return this.x;
+  }
+
+  public double getY() {
+    return this.y;
+  }
+
+  public String toString(){
+    return "(" + this.x + ", " + this.y + ")";       // NOTE toString format
+  }
+}
