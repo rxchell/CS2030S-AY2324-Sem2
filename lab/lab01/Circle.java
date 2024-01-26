babb84fdfc2253252d062db68beea9cd2f0a5094
Rachel <133939424+rxchell@users.noreply.github.com>
2024-01-26 17:36:01 +0800

    Create Circle.java

---
+++ lab/lab01/Circle.java
@@ -0,0 +1,42 @@
+class Circle {
+  /** The center of the circle. */
+  private Point c;
+
+  /** The radius of the circle (assume positive). */
+  private double r;
+
+  /**
+   * Constructor for a circle.  Takes in a center c and a 
+   * radius r (assume to be positive). 
+   *
+   * @param c The center of the new circle.
+   * @param r The radius of the new circle.
+   */
+  public Circle(Point c, double r) {
+    this.c = c;
+    this.r = r;
+  }
+
+  /**
+   * Checks if a given point p is contained within the circle.
+   *
+   * @param p The point to test.
+   * @return true if p is within this circle; false otherwise.
+   */
+  public boolean contains(Point p) {
+    // TODO;
+    double dx = p.getX() - this.c.getX();
+    double dy = p.getY() - this.c.getY();
+    double distanceSq = dx * dx + dy * dy;
+    return distanceSq <= this.r * this.r;
+  }
+
+  /**
+   * Return the string representation of this circle.
+   *
+   * @return The string representing of this circle.
+   */
+  public String toString() {
+    return "{ center: " + this.c + ", radius: " + this.r + " }";
+  }
+}
