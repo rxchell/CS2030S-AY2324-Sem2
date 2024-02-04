b012fc6e7b65a7d0386f8d8dceab8c42ebc020cd
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-04 23:10:20 +0800

    Update ServiceBeginEvent.java

--- lab/lab1/ServiceBeginEvent.java
+++ lab/lab1/ServiceBeginEvent.java
@@ -1,26 +1,26 @@
 public class ServiceBeginEvent extends Event {
-  public int customerId;
-  public double serviceTime;
-  public int counterId;
-  public boolean[] available;
+  private Customer customer;;
+  private double serviceTime;
+  private Counter counter;
+  private Counter[] available;
 
-  public ServiceBeginEvent(double time, int customerId, double serviceTime, int          counterId, boolean[] available) {
+  public ServiceBeginEvent(double time, Customer customer, double serviceTime, Counter counter, Counter[] available) {
     super(time);
-    this.customerId = customerId;
+    this.customer = customer;
     this.serviceTime = serviceTime;
-    this.counterId = counterId;
+    this.counter = counter;
     this.available = available;
   }
 
   @Override
   public String toString() {
-    return String.format(": Customer %d service begin (by Counter %d)", this.customerId, this.counterId) + super.toString();
+    return String.format(": Customer %d service begin (by Counter %d)", this.customer.getCustomerId(), this.counter.getCounterId()) + super.         toString();
   }
 
   @Override
   public Event[] simulate() {
-    this.available[this.counterId] = false;
+    this.counter.setIsAvailable(false);
     double endTime = this.getTime() + this.serviceTime;
-    return new Event[] { new ServiceEndEvent(endTime, this.customerId, this.serviceTime, this.counterId, this.available) };
+    return new Event[] { new ServiceEndEvent(endTime, this.customer, this.serviceTime, this.counter, this.available) };
   }
 }
