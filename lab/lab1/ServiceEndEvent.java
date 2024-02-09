3298408e83060db000d117bba6e0b3e409e742d6
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-09 14:14:32 +0800

    Update ServiceEndEvent.java

--- lab/lab1/ServiceEndEvent.java
+++ lab/lab1/ServiceEndEvent.java
@@ -4,26 +4,21 @@
 
 class ServiceEndEvent extends Event {
   private Customer customer;
-  private double serviceTime;
   private Counter counter;
-  private Counter[] available;
 
-  public ServiceEndEvent(double time, Customer customer, double serviceTime, Counter      counter, Counter[] available) {
-    super(time);
+  public ServiceEndEvent(Customer customer, Counter counter) {
     this.customer = customer;
-    this.serviceTime = serviceTime;
     this.counter = counter;
-    this.available = available;
   }
 
   @Override
   public String toString() {
-    return String.format(": Customer %d service done (by Counter %d)", this.customer.     getCustomerId, this.counter.getCounterId) + super.toString();
+    return String.format(": %s service done (by %s)", customer.toString(), counter.toString()) + super.toString();
   }
 
   @Override
   public Event[] simulate() {
-    this.counter.setIsAvailable(true);
-    return new Event[] { new DepartureEvent(this.getTime(), this.customer) };
+    this.counter.finish(customer);
+    return new Event[] { new DepartureEvent(this.customer) };
   }
 }
