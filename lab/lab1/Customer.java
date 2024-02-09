7df082f962a612c1f20b136015671617a5077305
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-09 14:13:12 +0800

    Update Customer.java

--- lab/lab1/Customer.java
+++ lab/lab1/Customer.java
@@ -1,18 +1,29 @@
+/** 
+ * @author Rachel Tai Ke Jia
+ **/
+
 class Customer {
-  
+
   /**
    * new instance id to store the identifier 
    * final as the identifier should not change
    **/
   private final int customerId; // identifier 
-  private static int lastId = 0; // id of the latest customer instance 
+  private double serviceTime;
+  private double arrivalTime;;
 
-  public Customer() {
-    this.customerId = Customer.lastId; 
-    Customer.lastId += 1;     // explicitly access lastId through 
+  public Customer(int customerId,double serviceTime, double arrivalTime, int noOfTotalCounters) {
+    this.customerId = customerId;
+    this.serviceTime = serviceTime;
+    this.arrivalTime = arrivalTime;
   }
 
-  public int getCustomerId() {
-    return this.customerId;
+  public int getEndTime(double arrivalTime) {
+    return this.serviceTime + arrivalTime;
   }
-} 
+
+  @Override
+  public String toString() {
+    return String.format("Customer %d", this.customerId);
+  }
+}
