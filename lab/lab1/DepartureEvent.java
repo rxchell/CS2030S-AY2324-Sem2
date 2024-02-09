615ae8f03e2c5b5757347469e07889d16efb7f0c
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-09 14:13:32 +0800

    Update DepartureEvent.java

--- lab/lab1/DepartureEvent.java
+++ lab/lab1/DepartureEvent.java
@@ -5,19 +5,17 @@
 public class DepartureEvent extends Event {
   private Customer customer;
 
-  public DepartureEvent(double time, Customer customer) {
-    super(time);
+  public DepartureEvent(Customer customer) {
     this.customer = customer;
   }
 
   @Override
   public String toString() {
-    return String.format(": Customer %d departed", this.customer.getCustomerId) + super.  toString();
+    return String.format(": %s departed", customer.toString()) + super.toString();
   }
 
   @Override
   public Event[] simulate() {
     // do nothing
-    return new Event[] {};
   }
 }
