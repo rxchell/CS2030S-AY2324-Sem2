c68e1a4049e71d885f85540ae18cd624fcb8ae51
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-09 14:11:36 +0800

    Update Bank.java

--- lab/lab1/Bank.java
+++ lab/lab1/Bank.java
@@ -1,1 +1,21 @@
+/**
+ * @author: Rachel Tai Ke Jia
+ * */
+
+class Bank {
+  private Counter[] counters;
+
+  public Bank(Counter[] counters){
+    this.counters = counters;
+  }
+
+  public int findAvailableCounter() {
+    for (int i = 0; i < this.counters.length; i++) {
+      if (this.counters[i].GetIsAvailable()) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
 
