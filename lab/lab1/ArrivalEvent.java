5b7d6137f904f640c2a65836921a082036b833ae
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-11 10:57:58 +0800

    Update ArrivalEvent.java

--- lab/lab1/ArrivalEvent.java
+++ lab/lab1/ArrivalEvent.java
@@ -6,7 +6,8 @@ class ArrivalEvent extends Event {
   private Customer customer;
   private Bank bank;
 
-  public ArrivalEvent(Customer customer, Bank bank) {
+  public ArrivalEvent(double time, Customer customer, Bank bank) {
+    super(time);
     this.customer = customer;
     this.bank = bank;
   }
@@ -20,9 +21,9 @@ class ArrivalEvent extends Event {
   public Event[] simulate() {
     int c = bank.findAvailableCounter();
     if (c == -1) {
-      return new Event[] { new DepartureEvent(this.customer) };
+      return new Event[] { new DepartureEvent(this.getTime(), this.customer) };
     } else {
-      return new Event[] { new ServiceBeginEvent(this.customer, this.bank.Counters[c]) };
+      return new Event[] { new ServiceBeginEvent(this.getTime(), this.customer, this.bank. getCounters()[c]) };
     }
   }
 }
