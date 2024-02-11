f54b7ab364a8e6101bcd631330ef64f23ecc221f
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-11 11:01:52 +0800

    Update ServiceBeginEvent.java

--- lab/lab1/ServiceBeginEvent.java
+++ lab/lab1/ServiceBeginEvent.java
@@ -6,19 +6,20 @@ class ServiceBeginEvent extends Event {
   private Customer customer;
   private Counter counter;
 
-  public ServiceBeginEvent(Customer customer, Counter counter) {
+  public ServiceBeginEvent(double time, Customer customer, Counter counter) {
+    super(time);
     this.customer = customer;
     this.counter = counter;
   }
 
   @Override
   public String toString() {
-    return String.format(": %s service begin by (%s)", this.customer.toString(), this.counter.toString()) + super.toString();
+    return String.format(": %s service begin by (%s)", this.customer.toString(), this.     counter.toString()) + super.toString();
   }
 
   @Override
   public Event[] simulate() {
     this.counter.serve(customer);
-    return new Event[] { new ServiceEndEvent(this.customer, this.counter) };
+    return new Event[] { new ServiceEndEvent(this.getTime(), this.customer, this.counter)  };
   }
 }
