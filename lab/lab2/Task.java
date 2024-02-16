59ddc55e6d0285c72f67e262c8c70463b91db093
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-16 11:41:35 +0800

    Update Task.java

--- lab/lab2/Task.java
+++ lab/lab2/Task.java
@@ -2,24 +2,6 @@
  * @author Rachel Tai Ke Jia
  **/
 
-class Task {
-  private int task;
-
-  public Task(int task) {
-    this.task = task;
-  }
-
-  public String getTask() {
-    if (task == 0) {
-      return "deposit";
-    } else {
-      return "withdrawal";
-    }
-  }
-
-  @Override
-  public String toString() {
-    return this.getTask();
-  }
+public abstract class Task {
+  public abstract String toString();
 }
-
