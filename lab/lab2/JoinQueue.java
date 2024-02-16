dee8e4fe371a16f6543f945eeab59eab0b7a0918
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-16 11:39:09 +0800

    Update JoinQueue.java

--- lab/lab2/JoinQueue.java
+++ lab/lab2/JoinQueue.java
@@ -4,23 +4,23 @@
 
 class JoinQueue extends Event {
   private Customer customer;
-  private Queue queue;
+  private Bank bank;
 
-  public JoinQueue(double time, Customer customer, Queue queue) {
+  public JoinQueue(double time, Customer customer, Bank bank) {
     super(customer.getArrivalTime());
     this.customer = customer;
-    this.queue = queue;
+    this.bank = bank;
   }
 
   @Override
   public String toString() {
     return super.toString() + String.format(": %s joined queue ",
-          this.customer.toString()) + this.queue.toString();
+          this.customer.toString()) + this.bank.queueString();
   }
 
   @Override
   public Event[] simulate() {
-    queue.enq(this.customer); 
+    this.bank.addCustomerToQueue(this.customer);
     return new Event[] {};
   }
 }
