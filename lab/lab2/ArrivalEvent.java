1928a8b2a8660a2d5454e93e75b794f23433e39a
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-16 11:34:33 +0800

    Update ArrivalEvent.java

--- lab/lab2/ArrivalEvent.java
+++ lab/lab2/ArrivalEvent.java
@@ -5,37 +5,33 @@
 class ArrivalEvent extends Event {
   private Customer customer;
   private Bank bank;
-  private Queue queue;
 
-  public ArrivalEvent(double time, Customer customer, Bank bank, Queue queue) {
+  public ArrivalEvent(double time, Customer customer, Bank bank) {
     super(time);
     this.customer = customer;
-    this.bank = bank;  
-    this.queue = queue;
+    this.bank = bank;
   }
 
   @Override
   public String toString() {
-    return super.toString() + String.format(": %s arrived ", 
-          customer.toString()) + queue.toString();
+    return super.toString() + String.format(": %s arrived ",
+          this.customer.toString()) + this.bank.queueString();
   }
 
   @Override
   public Event[] simulate() {
-    int c = bank.findAvailableCounter();
-    boolean q = queue.isFull();
+    int c = this.bank.findAvailableCounter();
+    boolean full = this.bank.isQueueFull();
 
     if (c == -1) {
-      if (q == true) {
+      if (full == true) {
         return new Event[] { new DepartureEvent(this.getTime(), this.customer) };
       } else {
-        return new Event[] { new JoinQueue(this.getTime(), this.customer, 
-            this.queue) };  
+        return new Event[] { new JoinQueue(this.getTime(), this.customer, this.bank) };
       }
     } else {
       return new Event[] { new ServiceBeginEvent(this.getTime(), this.customer,
-            this.bank.getCounters()[c], this.queue) };
+            this.bank.getCounters()[c], this.bank) };
     }
   }
 }
-
