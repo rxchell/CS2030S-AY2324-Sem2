dbb01e4cc46b7f25e1ff40a9f37f009c76c6abfc
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-16 11:40:24 +0800

    Update ServiceBeginEvent.java

--- lab/lab2/ServiceBeginEvent.java
+++ lab/lab2/ServiceBeginEvent.java
@@ -5,20 +5,20 @@
 class ServiceBeginEvent extends Event {
   private Customer customer;
   private Counter counter;
-  private Queue queue; 
+  private Bank bank;
 
-  public ServiceBeginEvent(double time, Customer customer, Counter counter, 
-      Queue queue) {
+  public ServiceBeginEvent(double time, Customer customer, Counter counter, Bank bank) {
     super(time);
     this.customer = customer;
     this.counter = counter;
-    this.queue = queue;
+    this.bank = bank;
   }
 
   @Override
   public String toString() {
-    return super.toString() + String.format(": %s %s begin (by %s)", 
-        this.customer.toString(), this.customer.getTask(), 
+    int t = this.customer.getTask();
+    return super.toString() + String.format(": %s %s begin (by %s)",
+        this.customer.toString(), this.customer.taskType(t),
         this.counter.toString());
   }
 
@@ -28,7 +28,7 @@ class ServiceBeginEvent extends Event {
     double endTime = this.getTime() + this.customer.getServiceTime();
     this.counter.serve(customer);
 
-    return new Event[] { new ServiceEndEvent(endTime, this.customer, 
-        this.counter, this.queue) };
+    return new Event[] { new ServiceEndEvent(endTime, this.customer,
+        this.counter, this.bank) };
   }
 }
