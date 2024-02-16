7c6c8dfbdb37f700a20a6acd832a22b495bfa8cb
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-16 11:34:12 +0800

    Create Withdrawal.java

---
+++ lab/lab2/Withdrawal.java
@@ -0,0 +1,15 @@
+/**
+ * @author Rachel Tai Ke Jia
+ **/
+
+class Withdrawal extends Task {
+  private int withdrawal;
+
+  public Withdrawal(int withdrawal) {
+    this.withdrawal = withdrawal;
+  }
+
+  public String toString() {
+    return "withdrawal";
+  }
+}
