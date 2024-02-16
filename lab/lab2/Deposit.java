7c45c54f7993380b7fde11ab08b87ca702fe1a15
Rachel <133939424+rxchell@users.noreply.github.com>
2024-02-16 11:33:52 +0800

    Create Deposit.java

---
+++ lab/lab2/Deposit.java
@@ -0,0 +1,15 @@
+/**
+ * @author Rachel Tai Ke Jia
+ **/
+
+class Deposit extends Task {
+  private int deposit;
+
+  public Deposit(int deposit) {
+    this.deposit = deposit;
+  }
+
+  public String toString() {
+    return "deposit";
+  }
+}
